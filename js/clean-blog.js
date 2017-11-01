5797a5a2ef6a1e3f8ea9b18d9d45e74b429502c6
Shirley <shirleykabir@gmail.com>
2017-11-01 18:54:28 -0400

    Push back js

--- js/clean-blog.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-      $("#work_link").click(function() {
-                        $('html, body').animate({
-                        scrollTop: $("#work").offset().top
-                        }, 2000);
-            });
-   });
-
