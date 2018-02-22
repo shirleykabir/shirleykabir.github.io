15679119cf6d6ce5f085f9151d224a8cf92757ac
Shirley <szk4@cornell.edu>
2018-02-21 22:46:13 -0500

    Add face :))

--- js/other.js
+++ js/other.js
@@ -4,4 +4,16 @@ $(function() {
                         scrollTop: $("#work").offset().top
                         }, 2000);
             });
+
+            $("#about_link").click(function() {
+                        $('html, body').animate({
+                        scrollTop: $("#about").offset().top
+                        }, 2000);
+            });
+
+            $("#projects_link").click(function() {
+                        $('html, body').animate({
+                        scrollTop: $("#projects").offset().top
+                        }, 2000);
+            });
 });
